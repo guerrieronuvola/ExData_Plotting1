ad8bf1bdb9d70e14443f2a6fded8ce2189e7a795
guerrieronuvola <terrytop@gmail.com>
2015-03-04 00:28:15 +0100

    secondo plot ok

---
+++ plot2.R
@@ -0,0 +1,11 @@
+# source("load_data.R")
+
+Sys.setlocale("LC_TIME", "English")
+
+png(filename = "plot2.png",
+    width = 480, height = 480, units = "px", bg = "white")
+
+plot(dataset$real_time, dataset$Global_active_power, type = "l", 
+	ylab = "Global Active Power (kilowatts)", xlab = "")
+
+dev.off()
